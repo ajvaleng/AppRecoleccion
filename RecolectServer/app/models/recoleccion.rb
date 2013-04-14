6b3c2c52a1b0d17f664d2b74a63d1cb60561d48e
Cristobal Lama <cristoballama@gmail.com>
2013-04-14 20:55:16 -0300

    Eliminado el directorio de RecolectServer para migrarlo al repositoriohttps://github.com/ajvaleng/RecolectServer #928311

--- RecolectServer/app/models/recoleccion.rb
+++
@@ -1,3 +0,0 @@
-class Recoleccion < ActiveRecord::Base
-  attr_accessible :lat, :llegada_paradero, :long, :nombre, :patente, :periodo, :personas_bajan, :presonas_suben, :puerta, :recorrido, :salida_paradero
-end
--- RecolectServer/test/unit/helpers/recoleccions_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class RecoleccionsHelperTest < ActionView::TestCase
-end
